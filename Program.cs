11f79cc3d192df2612ac05930ad03345447683d1
mitali2608 <mitalibawankule21@gmail.com>
2021-09-18 23:24:56 +0530

    Main Program Added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FibonacciSeries.GetfibonacciSeries();
+            //PerfectNumber.GetPerfectnumber();
+            //PrimeNumber.GetPrime();
+            //ReverseNumber.GetReverse();
+            CouponNumber.GetCoupon();
+            Console.ReadKey();
+        }
+    }
+}
