f8639e0c89816317520bbf262f2de15d767e2801
mitali2608 <mitalibawankule21@gmail.com>
2021-09-18 23:26:54 +0530

    FibonacciSeries Program Added

---
+++ FibonacciSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6
+{
+    class FibonacciSeries
+    {
+        public static void GetfibonacciSeries()
+        {
+            Console.WriteLine("Enter the value till fibonacci series needs to be generated");
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int i = 2;
+
+            while (i < num)
+            {
+                i = i + 1;
+                int c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
+
