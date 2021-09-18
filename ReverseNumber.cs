2245128f4cedf6658c34afd4b04ab6932753b3da
mitali2608 <mitalibawankule21@gmail.com>
2021-09-18 23:26:15 +0530

    ReverseNumber Program Added

---
+++ ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6
+{
+    class ReverseNumber
+    {
+		public static void GetReverse()
+		{
+			int num;
+			int reverse = 0;
+
+			Console.WriteLine("Enter the number : ");
+			num = int.Parse(Console.ReadLine());
+
+			while (num > 0)
+			{
+				reverse = reverse * 10 + num % 10;
+				num = num / 10;
+			}
+
+			Console.WriteLine("Reversed number : {0}", reverse);
+
+		}
+	}
+}
