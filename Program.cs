bbe597bb2be96cf105395cacc9f9b9a4f699dba3
Mitali Bawankule <88092878+mitali2608@users.noreply.github.com>
2021-09-20 00:36:16 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ namespace Day6
     {
         static void Main(string[] args)
         {
-            //FibonacciSeries.GetfibonacciSeries();
-            //PerfectNumber.GetPerfectnumber();
-            //PrimeNumber.GetPrime();
-            //ReverseNumber.GetReverse();
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            FibonacciSeries.GetfibonacciSeries();
+            PerfectNumber.GetPerfectnumber();
+            PrimeNumber.GetPrime();
+            ReverseNumber.GetReverse();
             CouponNumber.GetCoupon();
             Console.ReadKey();
+            watch.Stop();
+            var time = watch.ElapsedMilliseconds;
+            Console.WriteLine("Time Elapsed = " + time + "ms");
         }
     }
 }
