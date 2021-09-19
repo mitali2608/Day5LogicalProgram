020b2ea1756476e0d0b6fb6e8ef019b924e9708f
Mitali Bawankule <88092878+mitali2608@users.noreply.github.com>
2021-09-20 01:25:05 +0530

    Add files via upload

---
+++ CouponNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6
+{
+    class CouponNumber
+    {
+        public static void GetCoupon()
+        {
+            
+            Console.WriteLine("Enter the number of coupons to be generated");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int num1;
+            int coupon;
+            for (int i = 1; i <= n; i++)
+            {
+                num1 = random.Next(124, 3176);
+                coupon = (2372 * num1) % 100000;
+                Console.WriteLine("coupon {0}-[{1}]", i, coupon);
+            }
+
+        }
+    }
+}
+
